28fed8c1c816a8dc95a84f5a8862df2b29a3610f
Shlomi gross <shlomi10@gmail.com>
2023-09-22 16:52:14 +0300

    Add new test and edit tests

--- pages/Home_page.py
+++ pages/Home_page.py
@@ -25,7 +25,6 @@ class Home_page:
         self.__name_header = page.locator("tr th:nth-child(2)")
         self.__family_header = page.locator("tr th:nth-child(3)")
 
-
     def add_user(self, first_name: str, last_name: str):
         expect(self.__add_person_icon).to_be_enabled()
         self.__add_person_icon.click()
@@ -36,89 +35,95 @@ class Home_page:
         self.__filter_field.press('Enter')
 
     def verify_user_added(self, exp_first_name: str, exp_last_name: str) -> bool:
-        user_added = False
         for row in self.__table.all():
             first_name_cell = row.locator(self.__name_col)
             first_name = first_name_cell.text_content().strip().lower()
             last_name_cell = row.locator(self.__family_col)
             family_name = last_name_cell.text_content().strip().lower()
             if (first_name == exp_first_name.lower()) and (family_name == exp_last_name.lower()):
-                user_added = True
-        return user_added
+                return True
+        return False
+
+    def verify_data_in_table(self) -> bool:
+        for index in range(1, 11):
+            self.go_to_page_in_table(str(index))
+            for row in self.__table.all():
+                if len(self.__table.all()) != 10:
+                    return False
+                else:
+                    id = row.locator(self.__id_col).inner_text()
+                    name = row.locator(self.__name_col).inner_text()
+                    family = row.locator(self.__family_col).inner_text()
+                    if not name or not family or not id:
+                        return False
+        return True
 
     def validate_id_on_page(self, value: str) -> bool:
-        above_ten = False
         self.__filter_field.fill(value)
         self.__filter_field.press('Enter')
-        id_in_table = self.__table.all().pop().get_by_text("0").text_content()
-        if id_in_table == '50':
-            above_ten = True
-        return above_ten
+        id_in_table = self.__table.all().pop().locator(self.__id_col).text_content()
+        if int(id_in_table) == int(value) * 10:
+            return True
+        return False
 
     def go_to_page_in_table(self, value: str):
         self.__filter_field.fill(value)
         self.__filter_field.press('Enter')
 
     def validate_user_in_page_tenth(self) -> bool:
-        above_ten = False
-        id_in_table = self.__table.all().pop().get_by_text("0").text_content()
+        id_in_table = self.__table.all().pop().locator(self.__id_col).text_content()
         if id_in_table == '100':
-            above_ten = True
-        return above_ten
+            return True
+        return False
 
     def validate_user_in_page_one_after_invalid_page(self) -> bool:
-        above_ten = False
-        id_in_table = self.__table.all().pop().get_by_text("0").text_content()
+        id_in_table = self.__table.all().pop().locator(self.__id_col).text_content()
         if id_in_table == '10':
-            above_ten = True
-        return above_ten
+            return True
+        return False
 
     def validate_user_in_page_eleven(self) -> bool:
-        above_ten = False
-        id_in_table = self.__table.all().pop().get_by_text("1").text_content()
-        if id_in_table == '1':
-            above_ten = True
-        return above_ten
+        id_in_table = self.__table.all().pop().locator(self.__id_col).text_content()
+        if id_in_table == '110':
+            return True
+        return False
 
     def validate_page_in_table(self, value: str) -> bool:
         return self.validate_id_on_page(value)
 
     def validate_page_in_table_with_invalid_values(self, value: str) -> bool:
-        above_ten = False
         if self.validate_id_on_page(PAGE_TO_GO):
-            id_in_table = self.__table.all().pop().get_by_text("0").text_content()
+            id_in_table = self.__table.all().pop().locator(self.__id_col).text_content()
             self.__filter_field.fill(value)
             self.__filter_field.press('Enter')
             if id_in_table == '50':
-                above_ten = True
-        return above_ten
+                return True
+        return False
 
     def validate_previous_and_forward_arrows(self, value: str) -> bool:
-        above_ten = False
         self.__filter_field.fill(value)
         self.__filter_field.press('Enter')
-        id_in_table = self.__table.all().pop().get_by_text("0").text_content()
-        if id_in_table == '50':
-            above_ten = True
+        id_in_table = self.__table.all().pop().locator(self.__id_col).text_content()
+        if id_in_table != '50':
+            return False
         self.__next_page_button.click()
-        id_in_table = self.__table.all().pop().get_by_text("0").text_content()
-        if id_in_table == '60':
-            above_ten = True
+        id_in_table = self.__table.all().pop().locator(self.__id_col).text_content()
+        if id_in_table != '60':
+            return False
         self.__previous_page_button.click()
-        id_in_table = self.__table.all().pop().get_by_text("0").text_content()
-        if id_in_table == '50':
-            above_ten = True
-        return above_ten
+        id_in_table = self.__table.all().pop().locator(self.__id_col).text_content()
+        if id_in_table != '50':
+            return False
+        return True
 
     def verify_ten_rows_in_table(self, exp_rows: str) -> bool:
         return len(self.__table.all()) == exp_rows
 
     def verify_title_headers(self,page: str, exp_id: str, exp_name: str, exp_family: str) -> bool:
-        is_title = False
         self.go_to_page_in_table(page)
         if (self.__id_header.text_content() == exp_id and
             self.__name_header.text_content() == exp_name and
             self.__family_header.text_content() == exp_family):
-            is_title = True
-        return is_title
+            return True
+        return False
 
